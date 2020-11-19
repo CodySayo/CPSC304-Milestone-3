0e1581ba1d527fe1e994be74bbf7f5cfbeb7d05a
Coby <cobysayo@outlook.com>
2020-11-18 19:53:41 -0800

    forced to commit (only change is the oracle log in credentials)

--- MatchHistory.php
+++ MatchHistory.php
@@ -310,7 +310,7 @@
             // Your username is ora_(CWL_ID) and the password is a(student number). For example, 
 			// ora_platypus is the username and a12345678 is the password.
 
-            $db_conn = OCILogon("ora_odys722", "a52955390", "dbhost.students.cs.ubc.ca:1522/stu");
+            $db_conn = OCILogon("ora_obys", "a68031525", "dbhost.students.cs.ubc.ca:1522/stu");
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
